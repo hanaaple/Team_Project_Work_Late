e7d96bebef3713c02e6ad5792ef362e0c5c0fcb7
i-nooo <ssfoxs@kakao.com>
2021-04-05 14:02:11 +0900

    2021.04.05_rough completion

---
+++ app/src/main/java/com/example/team_project_work_late/ui/Activity/Frag1.java
@@ -0,0 +1,28 @@
+package com.example.team_project_work_late.ui.Activity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.team_project_work_late.R;
+
+public class Frag1 extends Fragment // Fragment 클래스를 상속받아야한다
+{
+
+    private View view;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.frag1,container,false);
+
+        return view;
+    }
+}
