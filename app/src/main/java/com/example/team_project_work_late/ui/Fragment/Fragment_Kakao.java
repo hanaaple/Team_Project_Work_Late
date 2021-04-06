3e653459660eef6e4a41b770841f722979fb0def
citytexi <heekyne66@gmail.com>
2021-04-06 22:57:25 +0900

    Fragment_Kakao 주석추가

--- app/src/main/java/com/example/team_project_work_late/ui/Fragment/Fragment_Kakao.java
+++ app/src/main/java/com/example/team_project_work_late/ui/Fragment/Fragment_Kakao.java
@@ -14,24 +14,37 @@ import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-
+/*
+ * @FileName Fragment_Kakao
+ * @date 21.04.06
+ * @role Fragment
+ * @Inheritance Fragment
+ * @Method onCreateView
+ * */
 
 public class Fragment_Kakao extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // findViewById를 위한 View 재 할당
         View v = inflater.inflate(R.layout.fragment__kakao, container, false);
 
+        // 카카오맵 설정
         MapView mapView = new MapView(getActivity());
         ViewGroup mapViewContainer = v.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
+        // 센터포인트 설정
         mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304),true);
 
+        // 줌 정도 설정
         mapView.setZoomLevel(4,true);
 
+        // 마커용 맵포인트 설정
         MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+
+        // 마커 설정 -> 기본적인 세팅값
         MapPOIItem marker = new MapPOIItem();
         marker.setItemName("Default Marker");
         marker.setTag(0);
