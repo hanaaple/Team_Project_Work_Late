a56ed5685c626dd2416c6602091aabdb1cac1a3a
hanaaple <hanaaple@ajou.ac.kr>
2021-04-17 19:24:52 +0900

    LoginActivity 이름 변경
    MainActivity -> LoginActivity

--- app/src/main/java/com/example/team_project_work_late/ui/Activity/LoginActivity.java
+++ app/src/main/java/com/example/team_project_work_late/ui/Activity/LoginActivity.java
@@ -21,33 +21,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.team_project_work_late.R;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInApi;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-//gps
+    //gps
     private Button Locationbutton;
     private TextView textView;
 
-    void CheckLocation(Location location){
+    void CheckLocation(Location location) {
         double longitude = location.getLongitude();     //위도
         double latitude = location.getLatitude();       //경도
         double altitude = location.getAltitude();       //고도
@@ -62,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient googleSignInClient;
     private FirebaseAuth mAuth;
     private static final int REQ_SIGN_GOOGLE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,9 +64,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         //gps
-        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);;
-        Locationbutton = (Button)findViewById(R.id.LocationButton);
-        textView = (TextView)findViewById(R.id.LocationText);
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ;
+        Locationbutton = (Button) findViewById(R.id.LocationButton);
+        textView = (TextView) findViewById(R.id.LocationText);
 
         //위도가 바뀔때마다 사용되는 Listioner
         LocationListener locationListener = new LocationListener() {
@@ -85,13 +81,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                    ActivityCompat.requestPermissions(LoginActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
                 } else {
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
-                    if(locationManager != null){
+                    if (locationManager != null) {
                         Log.d("GPSTracker", "LocationManger is Enable");
                         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if(location != null){
+                        if (location != null) {
                             CheckLocation(location);
                         }
                     }
@@ -99,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 //
-
 
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -112,14 +107,15 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         btn_google_login.setOnClickListener(v -> {
             Intent signInIntent = googleSignInClient.getSignInIntent();
             startActivityForResult(signInIntent, REQ_SIGN_GOOGLE);
         });
-        btn_google_logout.setOnClickListener(v ->{
+        btn_google_logout.setOnClickListener(v -> {
             googleSignInClient.signOut();
         });
     }
@@ -131,18 +127,19 @@ public class MainActivity extends AppCompatActivity {
 
         //여기서 resultCode가 0 or -1로 나오며 오류가 뜨는데 없어도 파이어베이스에 성공적으로 연동된다. 내 3시간 ㅇㄷ?
         //if(resultCode == REQ_SIGN_GOOGLE){
-            System.out.println("구글 성공");
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-            }
+        System.out.println("구글 성공");
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account.getIdToken());
+        } catch (ApiException e) {
+        }
 //        }else{
 //            System.out.println(resultCode);
 //            System.out.println(data);
 //        }
     }
+
     private void firebaseAuthWithGoogle(String idToken) {
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
         mAuth.signInWithCredential(credential)
@@ -150,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "성공", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "성공", Toast.LENGTH_SHORT).show();
                             System.out.println("성공");
                         } else {
                             System.out.println("실패");
