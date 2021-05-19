bdafb672abd85c2aa46281c066cbf99542f45ff1
Kim HyeGyeong <51890074+i-nooo@users.noreply.github.com>
2021-05-19 21:54:30 +0900

    marker_edit

--- app/src/main/java/com/example/team_project_work_late/ui/Fragment/Fragment_Kakao.java
+++ app/src/main/java/com/example/team_project_work_late/ui/Fragment/Fragment_Kakao.java
@@ -44,6 +44,7 @@ public class Fragment_Kakao extends Fragment {
         // 마커용 맵포인트 설정
         MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
 
+<<<<<<< Updated upstream
         // 마커 설정 -> 커스텀 마커
         MapPOIItem customMarker = new MapPOIItem();
         customMarker.setItemName("custom Marker");
@@ -56,6 +57,49 @@ public class Fragment_Kakao extends Fragment {
         customMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
 
         mapView.addPOIItem(customMarker);
+=======
+        // 마커 설정 -> 기본적인 세팅값
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
+
+
+        // 커스텀 마커 수정 필요 추가로 Dpstry도 마커 추가필요
+        for (BcyclLendData_responseBody_items item : bcyclLendData.getBcyclLendDataresponse().getBcyclLendDataresponseBody().getItems()){
+            if (!item.getLatitude().isEmpty()&& !item.getLongitude().isEmpty()){
+                MapPOIItem customMarker = new MapPOIItem();
+                customMarker.setItemName(item.getBcyclLendNm());
+                customMarker.setTag(1);
+                MapPoint point = MapPoint.mapPointWithGeoCoord(Double.valueOf(item.getLatitude()),Double.valueOf(item.getLongitude()));
+                customMarker.setMapPoint(point);
+//                customMarker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+                customMarker.setCustomImageResourceId(R.drawable.ic_baseline_location_on_24_lend);
+                customMarker.setCustomImageAutoscale(false); //기기 해상도에 상관없이 동일한 크키로 표시한다. true는 이미지 크기를 해상도에 맞게 조절함.
+                customMarker.setCustomImageAnchor(0.5f, 1.0f); //마커 이미지 기준점
+                customMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+                mapView.addPOIItem(customMarker);
+            }
+        }
+        for (BcyclDpstryData_responseBody_items item : bcyclDpstryData.getBcyclDpstryData_response().getBcyclDpstryData_responseBody().getItems()){
+            if (!item.getLatitude().isEmpty()&& !item.getLongitude().isEmpty()){
+                MapPOIItem customMarker = new MapPOIItem();
+                customMarker.setItemName(item.getDpstryNm());
+                customMarker.setTag(1);
+                MapPoint point = MapPoint.mapPointWithGeoCoord(Double.valueOf(item.getLatitude()),Double.valueOf(item.getLongitude()));
+                customMarker.setMapPoint(point);
+//                customMarker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+                customMarker.setCustomImageResourceId(R.drawable.ic_baseline_location_on_24_dpstry);
+                customMarker.setCustomImageAutoscale(false); //기기 해상도에 상관없이 동일한 크키로 표시한다. true는 이미지 크기를 해상도에 맞게 조절함.
+                customMarker.setCustomImageAnchor(0.5f, 1.0f); //마커 이미지 기준점
+                customMarker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+                mapView.addPOIItem(customMarker);
+            }
+        }
+>>>>>>> Stashed changes
 
         return v;
     }
